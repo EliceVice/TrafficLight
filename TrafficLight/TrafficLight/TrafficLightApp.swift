391b665c4cee6276a847a0e3f10fd19b64081c8a
EliceVice <eliceios@icloud.com>
2023-12-11 15:25:08 +0100

    Project Created

---
+++ TrafficLight/TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Andrei Isayenka on 11/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
